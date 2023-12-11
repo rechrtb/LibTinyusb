89cdfb39df411c0056afea65bf6d504de0730bb0
rechrtb <rechrtb@gmail.com>
2023-12-11 19:05:23 +0800

    Increase buffer sizes for better performance

--- src/tusb_config.h
+++ src/tusb_config.h
@@ -84,11 +84,11 @@
 #define CFG_TUD_VENDOR           (0)
 
 // CDC FIFO size of TX and RX
-#define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
-#define CFG_TUD_CDC_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 128)
+#define CFG_TUD_CDC_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 128)
 
 // CDC Endpoint transfer buffer size, more is faster
-#define CFG_TUD_CDC_EP_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_EP_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 128)
 
 #ifdef __cplusplus
  }
