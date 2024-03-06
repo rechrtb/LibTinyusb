b24788124abbe5596644f1cae2e39d3755199377
rechrtb <rechrtb@gmail.com>
2024-03-06 21:02:39 +0800

    Enable host mode in config

--- src/tusb_config.h
+++ src/tusb_config.h
@@ -57,7 +57,7 @@
 #endif
 
 //#define CFG_TUSB_RHPORT0_MODE     (OPT_MODE_DEVICE | OPT_MODE_FULL_SPEED)
-#define CFG_TUSB_RHPORT0_MODE     (OPT_MODE_DEVICE)
+#define CFG_TUSB_RHPORT0_MODE     (OPT_MODE_DEVICE | OPT_MODE_HOST)
 
 #ifndef CFG_TUSB_OS
 # define CFG_TUSB_OS              OPT_OS_FREERTOS
@@ -82,6 +82,8 @@
 #define CFG_TUD_MSC              (0)
 #define CFG_TUD_MIDI             (0)
 #define CFG_TUD_VENDOR           (0)
+
+#define CFG_TUH_MSC              (1)
 
 // CDC FIFO size of TX and RX
 #define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
