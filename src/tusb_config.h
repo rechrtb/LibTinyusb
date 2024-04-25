708170505632fa1ac76ec4e6cce84032ca015087
rechrtb <rechrtb@gmail.com>
2024-04-25 18:44:32 +0800

    Add tinyUSB configuration for host mass storage

--- src/tusb_config.h
+++ src/tusb_config.h
@@ -56,8 +56,12 @@
 # endif
 #endif
 
+# if (CFG_TUSB_MCU == OPT_MCU_SAME5X)
 //#define CFG_TUSB_RHPORT0_MODE     (OPT_MODE_DEVICE | OPT_MODE_FULL_SPEED)
 #define CFG_TUSB_RHPORT0_MODE     (OPT_MODE_DEVICE)
+# elif (CFG_TUSB_MCU == OPT_MCU_SAMX7X)
+#define CFG_TUSB_RHPORT0_MODE     (OPT_MODE_DEVICE | OPT_MODE_HOST)
+#endif
 
 #ifndef CFG_TUSB_OS
 # define CFG_TUSB_OS              OPT_OS_FREERTOS
@@ -82,6 +86,12 @@
 #define CFG_TUD_MSC              (0)
 #define CFG_TUD_MIDI             (0)
 #define CFG_TUD_VENDOR           (0)
+
+#define CFG_TUH_ENUMERATION_BUFSIZE 256
+#define CFG_TUH_HUB              (1)
+#define CFG_TUH_MSC              (1)
+#define CFG_TUH_DEVICE_MAX       (CFG_TUH_HUB ? 2 : 1)
+#define CFG_TUH_MSC_MAXLUN       1
 
 // CDC FIFO size of TX and RX
 #define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 128)
